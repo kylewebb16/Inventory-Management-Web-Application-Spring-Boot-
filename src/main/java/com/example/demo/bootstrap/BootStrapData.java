50633f91dca48f30ee0145a75d6ff11fa3542aef
Kyle Webb <kyle.jo.webb@gmail.com>
2025-08-16 21:46:20 -0400

    Start part D, incomplete

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -13,6 +13,7 @@ import com.example.demo.service.ProductServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,15 +39,20 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        if (productRepository.count() > 0 || partRepository.count() > 0) {
+            System.err.println("Database already contains data, bypassing bootstrapdata");
+            return;
+        }
 
-       /*
-        OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
-        o.setInv(5);
-        o.setPrice(20.0);
-        o.setId(100L);
-        outsourcedPartRepository.save(o);
+
+
+        OutsourcedPart barbellClamp= new OutsourcedPart();
+        barbellClamp.setCompanyName("Rogue Fitness");
+        barbellClamp.setName("Barbell Clamp");
+        barbellClamp.setInv(5);
+        barbellClamp.setPrice(20.0);
+        barbellClamp.setId(100L);
+        outsourcedPartRepository.save(barbellClamp);
         OutsourcedPart thePart=null;
         List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
         for(OutsourcedPart part:outsourcedParts){
@@ -54,18 +60,23 @@ public class BootStrapData implements CommandLineRunner {
         }
 
         System.out.println(thePart.getCompanyName());
-        */
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
+
         for(OutsourcedPart part:outsourcedParts){
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
 
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
+
+        Product squatRack= new Product("Power Squat Rack",799.99,30);
+        Product benchPress= new Product("Flat Bench Press Station",549.99,30);
+        Product deadLiftPlatform= new Product("Deadlift Platform",999.99,20);
+        Product dumbbellSet= new Product("Adjustable Dumbbell Set",750.0,25);
+        Product multiStationTower= new Product("Multi-Station Tower",1499.99,20);
+        productRepository.save(squatRack);
+        productRepository.save(benchPress);
+        productRepository.save(deadLiftPlatform);
+        productRepository.save(dumbbellSet);
+        productRepository.save(multiStationTower);
+
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
